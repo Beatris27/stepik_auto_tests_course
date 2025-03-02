43e7854932ca3469fdf5147dd7c6219ec6c93ace
PoruchikovaEA <elena.poruchikova@vitech.team>
2025-03-02 22:11:01 +0300

    4 урок

--- less 4/conftest.py
+++ less 4/conftest.py
@@ -3,20 +3,18 @@ from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 
 
-
 def pytest_addoption(parser):
-    parser.addoption('--language', action='store', default='en',
-                     help="Choose language: en, rus, fr etc.")
+    parser.addoption('--language', action='store', default='ru',
+                     help="Выберите предпочтительный язык сайта, например: fr")
 
 
 @pytest.fixture(scope="function")
 def browser(request):
-    print("\nstart browser for test..")
-    language = request.config.getoption("language")
+    user_language = request.config.getoption("language")
+    print("\nЗапуск браузера chrome для теста на языке {}".format(user_language))
     options = Options()
-    options.add_experimental_option('prefs', {'intl.accept_languages': language})
+    options.add_experimental_option('prefs', {'intl.accept_languages': user_language})
     browser = webdriver.Chrome(options=options)
-    browser.implicitly_wait(10)
     yield browser
-    print("\nquit browser..")
+    print("\nБраузер закрывается..")
     browser.quit()
--- less 4/test_main_page.py
+++ less 4/test_main_page.py
@@ -1,37 +1,23 @@
-from pages.main_page import MainPage
+from pages.basket_page import BasketPage
 from pages.login_page import LoginPage
-from selenium.webdriver.chrome.webdriver import WebDriver
+from pages.main_page import MainPage
 
-link= "http://selenium1py.pythonanywhere.com/"
 
-def test_guest_can_go_to_login_page(browser):
-    page = MainPage(browser, link)
-    page.open()
-    login_page = page.go_to_login_page()
-    login_page.should_be_login_page()
+class TestLoginFromMainPage:
+    link = "https://selenium1py.pythonanywhere.com/"
 
-def test_guest_should_see_login_link(browser):
-    page = MainPage(browser, link)
-    page.open()
-    page.should_be_login_link()
+    def test_guest_can_go_to_login_page(self, browser):
+        page = MainPage(browser, self.link)
+        page.open()
+        page.should_be_login_link()
+        page.go_to_login_page()
+        login_page = LoginPage(browser, browser.current_url)
+        login_page.should_be_login_page()
 
-def test_guest_should_see_login_page_sigin_form(browser):
-    page = MainPage(browser, link)
-    page.open()
-    page.go_to_login_page()
-    loginPage = LoginPage(browser, WebDriver.current_url)
-    loginPage.should_be_login_form()  
-
-def test_guest_should_see_login_in_url(browser):
-    page = MainPage(browser, link)
-    page.open()
-    page.go_to_login_page()
-    loginPage = LoginPage(browser, WebDriver.current_url)
-    loginPage.should_be_login_url() 
-
-def test_guest_should_see_login_page_reg_form(browser):
-    page = MainPage(browser, link)
-    page.open()
-    page.go_to_login_page()
-    loginPage = LoginPage(browser, WebDriver.current_url)
-    loginPage.should_be_register_form()  
+    def test_guest_cant_see_product_in_basket_opened_from_main_page(self, browser):
+        main_page = MainPage(browser, self.link)
+        main_page.open()
+        main_page.go_to_basket_page()
+        basket_page = BasketPage(browser, browser.current_url)
+        basket_page.should_not_be_items()
+        basket_page.should_be_msg_about_basket_is_empty()
